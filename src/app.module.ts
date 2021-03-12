23e575352d0d4435caebbce754077f9a8d05a9ec
miguelchap <miachaparror@correo.udistrital.edu.co>
2021-03-11 21:16:30 -0500

    Added cars module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Cars } from './cars/cars.entity';
+import { CarsModule } from './cars/cars.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '12345',
+      database: 'transportation',
+      entities: [Cars],
+      synchronize: true,
+    }),
+    CarsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Get, Param, Post, Req, Request } from '@nestjs/common';
+import { CarsService } from './cars.service';
+
+@Controller('cars')
+export class CarsController {
+
+    constructor(private readonly carService: CarsService) { }
+
+    @Get()
+    findAll(@Req() request: Request) : {} {
+        return this.carService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param() param):{}{
+        return this.carService.findOne(param.id);
+    }
+}
